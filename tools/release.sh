33377ebd9afaade8e1713a66a7d1588ed3e60778
Matthias Heinz <heinz.matthias.3@gmail.com>
2019-09-20 08:58:45 +0200

    Change tar release command to work for GNU tar

--- tools/release.sh
+++ tools/release.sh
@@ -3,4 +3,4 @@
 rm -rf "dist"
 mkdir "dist"
 zip -r -X --exclude="*.DS_Store*" "dist/eisvogel-physics-${1}.zip" "examples" "eisvogel-physics.tex" "icon.png" "LICENSE" "README.md" "CHANGELOG.md"
-tar --exclude="*.DS_Store*" --include="examples" --include="eisvogel-physics.tex" --include="icon.png" --include="LICENSE" --include="README.md" --include="CHANGELOG.md" -zcvf "dist/eisvogel-physics-${1}.tar.gz" *
+tar --exclude="*.DS_Store*" "examples" "eisvogel-physics.tex" "icon.png" "LICENSE" "README.md" "CHANGELOG.md" -zcvf "dist/eisvogel-physics-${1}.tar.gz" *
