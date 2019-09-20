6d2cce3a6e515567da403964e92e3764e886a7e9
Matthias Heinz <heinz.matthias.3@gmail.com>
2019-09-20 08:26:04 +0200

    Complete renaming from eisvogel to eisvogel-physics

--- tools/release.sh
+++ tools/release.sh
@@ -2,5 +2,5 @@
 
 rm -rf "dist"
 mkdir "dist"
-zip -r -X --exclude="*.DS_Store*" "dist/Eisvogel-${1}.zip" "examples" "eisvogel.tex" "icon.png" "LICENSE" "README.md" "CHANGELOG.md"
-tar --exclude="*.DS_Store*" --include="examples" --include="eisvogel.tex" --include="icon.png" --include="LICENSE" --include="README.md" --include="CHANGELOG.md" -zcvf "dist/Eisvogel-${1}.tar.gz" *
+zip -r -X --exclude="*.DS_Store*" "dist/eisvogel-physics-${1}.zip" "examples" "eisvogel-physics.tex" "icon.png" "LICENSE" "README.md" "CHANGELOG.md"
+tar --exclude="*.DS_Store*" --include="examples" --include="eisvogel-physics.tex" --include="icon.png" --include="LICENSE" --include="README.md" --include="CHANGELOG.md" -zcvf "dist/eisvogel-physics-${1}.tar.gz" *
